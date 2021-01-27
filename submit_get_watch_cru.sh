ef8841e8ca20638685b6dc1368265344d2c58cd9
stineb <benjamin.stocker@gmail.comm>
2021-01-27 09:43:01 +0100

    don't remember

--- submit_get_watch_cru.sh
+++ submit_get_watch_cru.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bsub -n 1 -W 72:00 -u bestocke -J get_watch_cru -R "rusage[mem=25000]" "Rscript --vanilla rscript_get_watch_cru.R"
+bsub -W 72:00 -u bestocke -J get_watch_cru -R "rusage[mem=25000]" "Rscript --vanilla rscript_get_watch_cru.R"
