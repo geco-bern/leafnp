f7389486082851e0c089421d942c24d085dd5d93
stineb <benjamin.stocker@gmail.comm>
2021-03-12 18:23:24 +0100

    asdf

--- submit_ingest_run_rsofun.sh
+++ submit_ingest_run_rsofun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-njobs=100
-for ((n=1;n<=${njobs};n++)); do
-    echo "Submitting chunk number $n ..."
-    bsub -W 72:00 -u bestocke -J "ingest_run_rsofun $n" -R "rusage[mem=72000]" "Rscript --vanilla rscript_ingest_run_rsofun.R $n $njobs"
-done
+# njobs=100
+# for ((n=1;n<=${njobs};n++)); do
+#     echo "Submitting chunk number $n ..."
+#     bsub -W 72:00 -u bestocke -J "ingest_run_rsofun $n" -R "rusage[mem=72000]" "Rscript --vanilla rscript_ingest_run_rsofun.R $n $njobs"
+# done
 
-# bsub -W 72:00 -u bestocke -J "ingest_run_rsofun 1" -R "rusage[mem=48000]" "Rscript --vanilla rscript_ingest_run_rsofun.R 1 100"
+bsub -W 72:00 -u bestocke -J "ingest_run_rsofun 44" -R "rusage[mem=48000]" "Rscript --vanilla rscript_ingest_run_rsofun.R 44 100"
