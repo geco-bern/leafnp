e537bfb5a555bad359a86e4d99a31a3ad32990cd
stineb <b.stocker@imperial.ac.uk>
2021-03-13 19:07:35 +0100

    added file back

---
+++ rscript_ingest_run_rsofun.R
@@ -0,0 +1,81 @@
+#!/usr/bin/env Rscript
+# args = commandArgs(trailingOnly=TRUE)
+
+args <- c(44, 100)
+
+library(dplyr)
+library(purrr)
+library(tidyr)
+library(magrittr)
+library(rlang)
+library(lubridate)
+library(ingestr)
+library(rsofun)
+library(rbeni)
+library(pryr)
+
+source("R/ingest_run_rsofun.R")
+
+reduce_mem <- FALSE
+n_reduce <- 9
+
+## read sites data frame
+df_sites <- read_csv("~/leafnp/data/df_sites.csv") %>%
+  mutate(idx = 1:n()) %>% 
+  mutate(chunk = rep(1:as.integer(args[2]), each = (nrow(.)/as.integer(args[2])), len = nrow(.)))
+
+## split sites data frame into (almost) equal chunks
+list_df_split <- df_sites %>% 
+  group_by(chunk) %>% 
+  group_split()
+
+# ## test
+# df_test <- list_df_split %>% bind_rows()
+# all_equal(df_test, df_sites)
+
+## retain only the one required for this chunk
+df_sites_sub <- list_df_split[[as.integer(args[1])]]
+
+print("This chunk contains these rows of the full site data frame:")
+print(df_sites_sub$idx)
+
+##------------------------------------------------------------------------
+## ingest forcing data, run P-model, and get climate indeces at once
+##------------------------------------------------------------------------
+filn <- paste0("data/df_pmodel_ichunk_", args[1], "_", args[2], ".RData")
+if (!file.exists(filn)){
+  if (reduce_mem){
+    
+    ## split into chunks
+    nsites_per_chunk <- nrow(df_sites_sub)/n_reduce
+    list_df_split <- split(df_sites_sub, seq(nrow(df_sites_sub)) %/% nsites_per_chunk)
+    
+    df_pmodel <- tibble()
+    for (idx in seq(n_reduce)){
+      print("------------------------------------")
+      print(paste("Sub-chunk", idx))
+      print("------------------------------------")
+      df_pmodel <- bind_rows(
+        df_pmodel,
+        ingest_run_rsofun(list_df_split[[idx]], ichunk = args[1], totchunk = args[2], subchunk = idx, verbose = FALSE)
+      )
+    }
+    save(df_pmodel, file = filn)
+    
+  } else {
+    df_pmodel <- ingest_run_rsofun(df_sites_sub, ichunk = args[1], totchunk = args[2], subchunk = "", verbose = FALSE)
+    save(df_pmodel, file = filn)
+  }
+} else {
+  print(paste("File exists already: ", filn))
+}
+
+# ## memory profiling
+# library(pryr)
+# library(profvis)
+# prof <- profvis({
+#   df_pmodel <- ingest_run_rsofun(df_sites_sub, ichunk = args[1], totchunk = args[2])
+# })
+#
+# print(prof)
+
