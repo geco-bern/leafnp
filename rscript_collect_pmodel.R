c16582099185e459c507389550464112a95f7edb
Benjamin Stocker <benjamin.stocker@gmail.com>
2021-03-18 15:23:49 +0100

    asdf

--- rscript_collect_pmodel.R
+++ rscript_collect_pmodel.R
@@ -2,11 +2,15 @@ library(tidyverse)
 
 open_pmodel <- function(ichunk){
   path <- paste0("data/df_pmodel_ichunk_", ichunk, "_100.RData")
-  load(path)
+  if (file.exists(path)){
+    load(path)
+  } else {
+    df_pmodel <- tibble()
+  }
   return(df_pmodel)
 }
 
-df <- purrr::map_dfr(as.list(seq(3)),
+df <- purrr::map_dfr(as.list(seq(100)),
                      ~open_pmodel(.))
 
 write_csv(df, file = "data/df_pmodel.csv")
